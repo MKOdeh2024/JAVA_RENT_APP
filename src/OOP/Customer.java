39cb66f95e8db6872caaf2268e73d240608c6bfe
Mohammad Khaled Mohammad Odah <137823341+MKOdeh2024@users.noreply.github.com>
2024-07-24 21:33:56 +0300

    Update Customer.java

--- src/OOP/Customer.java
+++ src/OOP/Customer.java
@@ -3,25 +3,30 @@ package OOP;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Customer {
-     String name;
-     List<Contract> contracts;
+public class Customer  extends Person {
+	private List<Contract> contracts;
+    private double balance;
 
-    public Customer(String name) {
-        this.name = name;
+    public Customer(String name, double initialBalance) {
+        super(name);
         this.contracts = new ArrayList<>();
+        this.balance = initialBalance;
     }
 
-    public String getName() {
-        return name;
-    }
 
-    public void addContract(Contract contract) {
+	public void rent(Owner owner, Building building, int rentalPeriod, double price) {
+        if (balance < price) {
+            throw new IllegalStateException("Insufficient funds to rent the building");
+        }
+
+        Contract contract = Contract.generateContract(this, owner, building, rentalPeriod, price);
         contracts.add(contract);
+        balance -= price;
+        System.out.println("Rent paid. Remaining balance: " + balance + " USD");
     }
 
     public void displayContracts() {
-        for(Contract contract : contracts) {
+        for (Contract contract : contracts) {
             contract.displayContract();
         }
     }
