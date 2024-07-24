03eee9411cb29c948f590727bb6998250bd174a5
Mohammad Khaled Mohammad Odah <137823341+MKOdeh2024@users.noreply.github.com>
2024-07-24 21:32:43 +0300

    Create Person.java

---
+++ src/OOP/Person.java
@@ -0,0 +1,15 @@
+package OOP;
+
+public abstract class Person {
+	
+	    protected String name;
+	  
+	    public Person(String name) {
+	        this.name = name;
+	    }
+
+	    public String getName() {
+	        return name;
+	    }
+}
+
